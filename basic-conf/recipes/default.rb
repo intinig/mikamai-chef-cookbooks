2efc49c917aa95352655796415bb6bab42f9dc3e
Giovanni Intini <giovanni@mikamai.com>
2012-10-27 13:23:31 +0200

    almost there

--- basic-conf/recipes/default.rb
+++ basic-conf/recipes/default.rb
@@ -7,7 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w(intinig pilu racco fauno).each do |u|
+include_recipe "apt"
+
+execute "dpkg-reconfigure grub-pc" do
+  environment "DEBIAN_FRONTEND"  => "noninteractive"
+  creates "/etc/chef/grub-reconfigured.semaphore"
+end
+
+execute "apt-get upgrade -y"
+
+%w(intinig).each do |u|
   user u do
     action :create
     home "/home/#{u}"
@@ -34,4 +43,57 @@
     owner u
     group u
   end
+end
+
+user "deploy" do
+  action :create
+  home "/var/apps"
+  shell "/bin/bash"
+  supports :manage_home => true
+end
+
+directory "/var/apps" do
+  mode 0755
+end
+
+directory "/var/apps/.ssh" do
+  action :create
+  owner "deploy"
+  group "deploy"
+  mode 0740
+end
+
+execute 'ssh-keygen -t dsa -f /var/apps/.ssh/id_dsa -N ""' do
+  user "deploy"
+  creates "/var/apps/.ssh/id_dsa"
+end
+
+package "build-essential" do
+  action :install
+end
+
+package "libxslt-dev" do
+  action :install
+end
+
+package "libxml2-dev" do
+  action :install
+end
+
+gem_package "github-key-upload" do
+  action :install
+  options("--no-ri --no-rdoc")
+end
+
+github_creds = Chef::EncryptedDataBagItem.load("passwords", "github")
+github_command_string = "github-key-upload -k /var/apps/.ssh/id_dsa.pub -u #{github_creds["user"]} -P #{github_creds["password"]} -t #{node.ec2.instance_id}"
+
+execute github_command_string + " -C" do
+  notifies :run, "execute[#{github_command_string}]", :immediately
+  returns 1
+  ignore_failure true
+end
+
+execute github_command_string do
+  action :nothing
 end
