674ac7009f4619172fa27b570982eb6d9537817a
Giovanni Intini <giovanni@mikamai.com>
2012-11-13 13:03:58 +0100

    new recipe

---
+++ basic-conf/recipes/databases.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: mysql
+# Recipe:: databases
+#
+# Copyright 2012, MIKAMAI
+#
+# All rights reserved - Do Not Redistribute
+#
+
+mysql_connection_info = {:host => "localhost", :username => "root", :password => node['mysql']['server_root_password']}
+
+node.mysql.databases.each do |db|
+  mysql_database db["name"] do
+    connection mysql_connection_info
+    action :create
+  end
+
+
+  mysql_database_user db["user"] do
+    connection mysql_connection_info
+    password db["password"]
+    database_name db["name"]
+  end
+end
